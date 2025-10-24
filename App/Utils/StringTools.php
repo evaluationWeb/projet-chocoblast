a037d126b669027e9d07aa527ce47fde288cc9f0
mithridatem <mathieumithridate@adrar-formation.com>
2025-10-24 20:51:42 +0200

    docs : add php-doc App\Utils\StringTools

--- App/Utils/StringTools.php
+++ App/Utils/StringTools.php
@@ -15,25 +15,29 @@ class StringTools
         return htmlspecialchars(strip_tags(trim($value)), ENT_NOQUOTES);
     }
 
-
+    /**
+     * Méthode pour sanitize un tableau
+     * @param array $data 
+     * @return array $data retourne le tableau sanitize
+     */
     public static function sanitize_array(array $data): array
-{   
-    //Boucle pour itérer sur le tableau $data
-    foreach ($data as $key => $value) {
-        //Test si la valeur est de type string
-        if (gettype($value) == "string") {
-            $data[$key] = self::sanitize($value);
-        }
-        //Test si $value est un tableau
-        if (gettype($value) == "array") {
-            //nettoyage du sous tableau
-            foreach ($value as $cle => $contenu) {
-               $data[$key][$cle] = self::sanitize($contenu);
+    {
+        //Boucle pour itérer sur le tableau $data
+        foreach ($data as $key => $value) {
+            //Test si la valeur est de type string
+            if (gettype($value) == "string") {
+                $data[$key] = self::sanitize($value);
+            }
+            //Test si $value est un tableau
+            if (gettype($value) == "array") {
+                //nettoyage du sous tableau
+                foreach ($value as $cle => $contenu) {
+                    $data[$key][$cle] = self::sanitize($contenu);
+                }
             }
         }
+        return $data;
     }
-    return $data;
-}
     /**
      * Méthode qui retourne l'extension d'un fichier
      * @param string $file nom du fichier
@@ -44,7 +48,11 @@ class StringTools
         return substr(strrchr($file, '.'), 1);
     }
 
-    //Méthode qui convertie une chaine de caractéres en UTF-8
+    /**
+     * Méthode qui convertie une chaine de caractéres en UTF-8
+     * @param string $str chaine à encoder en UTF8
+     * @return string $str chaine encodée en UTF8
+    * */
     public static function utf8Encode(string $str): string
     {
 
